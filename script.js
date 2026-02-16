b83af903ecefdadef2c8de8b08dcc589b97162c5
Leo <leorwharfe@gmail.com>
2026-02-16 12:29:43 +0000

    Verision 1 (Index.html, Style.css, Script.js) contains {hero, about, gallery, and form}

---
+++ script.js
@@ -0,0 +1,24 @@
+const about = document.getElementById('about');
+const photos = document.getElementById('photos-gallery');
+
+const track = document.getElementById('track');
+const trackItems = document.querySelectorAll('#track-item');
+
+const leftButton = document.getElementById('left-button');
+const rightButton = document.getElementById('right-button');
+
+// Duplicate track items for seamless looping
+
+
+// plan: photos section where images are in a horizontal loop, with description text below each image
+// the loop should incriment between each image, pausing briefly for each image (image should be in the centre) before moving to the next one
+// the loop should be infinite, so when the last image is reached, it should loop back to the first image seamlessly
+// images that are not in the centre should be slightly faded out to give focus to the centred image, and scale 0.8x
+// there should be arrow buttons on either side of the loop to manually move to the next/previous image
+// the loop should also pause on hover, and resume when hover ends
+
+// Bonus: add touch/swipe support for mobile devices
+
+
+
+
